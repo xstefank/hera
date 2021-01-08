6dbfba2b635535c894c0a76ff46d731c9adf23f0
Romain Pelisse <belaran@redhat.com>
2021-01-08 16:59:05 -0500

    Path to volume source

--- run.sh
+++ run.sh
@@ -2,9 +2,14 @@
 readonly BUILD_PODMAN_IMAGE=${BUILD_PODMAN_IMAGE:-'fedora'}
 readonly CONTAINER_COMMAND=${CONTAINER_COMMAND:-'/home/jenkins/run.sh'}
 
+readonly VOLUME_HOME='/home/jenkins/workspace'
+
 source ${HERA_HOME}/library.sh
 
 is_defined "${WORKSPACE}" "No workspace provided." 1
 is_dir "${WORKSPACE}" "Workspace provided is not a dir: ${WORKSPACE}" 2
 
-run_ssh "podman run -v ${WORKSPACE}:${WORKSPACE}:rw -d ${BUILD_PODMAN_IMAGE} '${CONTAINER_COMMAND}'"
+readonly PROJECT_NAME=$(basename "../${WORKSPACE}")
+readonly VOLUME_PATH="${VOLUME_HOME}/${PROJECT_NAME}"
+
+run_ssh "podman run -v "${VOLUME_PATH}:${WORKSPACE}:rw" -d ${BUILD_PODMAN_IMAGE} '${CONTAINER_COMMAND}'"
