6332bcc8dda725def596759b817b13b6bc6503de
Romain Pelisse <belaran@redhat.com>
2021-01-08 17:14:10 -0500

    Add library import build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 readonly CID=${1}
 readonly BUILD_SCRIPT=${2}
 
+source "${HERA_HOME}/library.sh"
+
 is_defined "${CID}" 'No contained id provided' 1
 is_defined "${BUILD_SCRIPT}" 'No build script provided' 2
 
