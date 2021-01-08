41b5e7e0878af7e9b609b228ad9ac5359ece2e23
Romain Pelisse <belaran@redhat.com>
2021-01-08 17:01:17 -0500

    Tweak path to run.sh script for container

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 readonly BUILD_PODMAN_IMAGE=${BUILD_PODMAN_IMAGE:-'fedora'}
-readonly CONTAINER_COMMAND=${CONTAINER_COMMAND:-'/home/jenkins/run.sh'}
-
 readonly VOLUME_HOME='/home/jenkins/workspace'
 
 source ${HERA_HOME}/library.sh
@@ -9,6 +7,7 @@ source ${HERA_HOME}/library.sh
 is_defined "${WORKSPACE}" "No workspace provided." 1
 is_dir "${WORKSPACE}" "Workspace provided is not a dir: ${WORKSPACE}" 2
 
+readonly CONTAINER_COMMAND=${CONTAINER_COMMAND:-"${WORKSPACE}/hera/wait.sh"}
 readonly PROJECT_NAME=$(basename "../${WORKSPACE}")
 readonly VOLUME_PATH="${VOLUME_HOME}/${PROJECT_NAME}"
 
---
+++ wait.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+readonly BUILD_JOB_TIMEOUT=${BUILD_JOB_TIMEOUT:-'120'}
+
+sleep 120
