7fc40c48e0e0c1657c1f079587d3830ff8bc6d3b
Romain Pelisse <belaran@redhat.com>
2021-01-09 12:40:11 -0500

    Rework stop to simply ignore errors

--- stop.sh
+++ stop.sh
@@ -2,7 +2,4 @@
 source "${HERA_HOME}/library.sh"
 
 is_defined "${CID}" "No container ID provided"
-is_cid_running=$(run_ssh "podman ps --filter=id=${CID} | sed -e /CONTAIN/d | wc -l")
-if [ ${is_cid_running} -gt 0 ]; then
-  run_ssh "podman stop ${CID}"
-fi
+run_ssh "podman stop -i ${CID}"
