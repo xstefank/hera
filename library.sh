d8d4e0e3109b0a46ec62da5c859c026e1a5d8454
Romain Pelisse <belaran@gmail.com>
2021-01-25 16:48:43 +0100

    Fixes all shellcheck warnings on library.sh

--- library.sh
+++ library.sh
@@ -1,64 +1,85 @@
 #!/bin/bash
+readonly HERA_SSH_KEY=${HERA_SSH_KEY}
+readonly HERA_HOSTNAME=${HERA_HOSTNAME}
+readonly HERA_USERNAME=${HERA_USERNAME}
+readonly HERA_SSH_OPTIONS=${HERA_SSH_OPTIONS}
+set -u
 
 is_defined() {
-  local var=${1}
-  local msg=${2:-'A required value is missing'}
-  local status=${3:-'1'}
-  
+  local var
+  local msg
+  local status
+
+  var=${1}
+  msg=${2:-'A required value is missing'}
+  status=${3:-'1'}
+
   if [ -z "${var}" ]; then
     echo "${msg}"
-    exit ${status}
+    exit "${status}"
   fi
 }
 
 is_dir() {
-  local path=${1}
-  local msg=${2:-'Invalid - not a directory'}
-  local status=${3:-'1'}
+  local path
+  local msg
+  local status
+
+  path=${1}
+  msg=${2:-'Invalid - not a directory'}
+  status=${3:-'1'}
 
   if [ ! -d "${path}" ]; then
     echo "${msg}"
-    exit ${status}
+    exit "${status}"
   fi
 }
 
 exists() {
-  local path=${1}
-  local msg=${2:-'File does not exists'}
-  local status=${3:-'1'}
+  local path
+  local msg
+  local status
+
+  path=${1}
+  msg=${2:-'File does not exists'}
+  status=${3:-'1'}
 
   if [ ! -e "${path}" ]; then
     echo "${msg}"
-    exit ${status}
+    exit "${status}"
   fi
 }
 
 is_executable() {
-  local path=${1}
-  local msg=${2:-'File is not executable'}
-  local status=${3:-'1'}
+  local path
+  local msg
+  local status
+
+  path=${1}
+  msg=${2:-'File is not executable'}
+  status=${3:-'1'}
 
   if [ ! -x "${path}" ]; then
     echo "${msg}"
-    exit ${status}
+    exit "${status}"
   fi
 }
 
 run_ssh() {
-  local hera_ssh_key=${HERA_SSH_KEY}
-  local hera_hostname=${HERA_HOSTNAME}
-  local hera_username=${HERA_USERNAME}
+  local ssh_options
 
-  local ssh_options="-o StrictHostKeyChecking=no ${HERA_SSH_OPTIONS}"
+  ssh_options="-o StrictHostKeyChecking=no ${HERA_SSH_OPTIONS}"
 
-# to impl  
-#  if [ -n "${hera_ssh_key}" ]; then
-#    exists "${hera_ssh_key}" "Provided path to SSH_KEY does not exists: ${hera_ssh_key}." 667
-#  fi
-  is_defined "${hera_hostname}" 'Please define env var HERA_HOSTNAME.' 668
-  is_defined "${hera_username}" 'Please define env var HERA_USERNAME' 669
+  if [ -n "${HERA_SSH_KEY}" ]; then
+    exists "${HERA_SSH_KEY}" "Provided path to SSH_KEY does not exists: ${HERA_SSH_KEY}." 667
+    ssh_options="${ssh_options} -i ${HERA_SSH_KEY}"
+  fi
 
+  is_defined "${HERA_HOSTNAME}" 'Please define env var HERA_HOSTNAME.' 668
+  is_defined "${HERA_USERNAME}" 'Please define env var HERA_USERNAME' 669
+
+  # shellcheck disable=SC2086,SC2029
   ssh ${ssh_options} \
-      "${hera_username}@${hera_hostname}"\
+      "${HERA_USERNAME}@${HERA_HOSTNAME}"\
       "${@}"
 }
