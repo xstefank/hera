caed9372a34ca67a1cd951146ecddb469d288dc0
Romain Pelisse <belaran@gmail.com>
2021-01-25 16:48:51 +0100

    Fixes all shellcheck warnings on run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 readonly BUILD_PODMAN_IMAGE=${BUILD_PODMAN_IMAGE:-'ubi8-jdk8'}
 readonly JENKINS_HOME_DIR=${JENKINS_HOME_DIR:-'/home/jenkins/'}
+readonly JOB_NAME=${JOB_NAME}
+readonly BUILD_ID=${BUILD_ID}
 
 add_parent_volume_if_provided() {
   if [ -n "${PARENT_JOB_VOLUME}" ]; then
@@ -9,14 +11,19 @@ add_parent_volume_if_provided() {
 }
 
 container_name() {
-  local job_name=$( echo "${1}" | sed -e 's; *;;g')
-  local build_id=$( echo "${2}" | sed -e 's; *;;g' )
-  local name_prefix=${3:-'automaton-slave'}
+  local job_name
+  local build_id
+  local name_prefix
+
+  job_name=${1// /}
+  build_id=${2// /}
+  name_prefix=${3:-'automaton-slave'}
 
   echo "${name_prefix}-${job_name}-${build_id}"
 }
 
-source ${HERA_HOME}/library.sh
+# shellcheck source=./library.sh
+source "${HERA_HOME}"/library.sh
 
 is_defined "${WORKSPACE}" "No WORKSPACE provided." 1
 is_dir "${WORKSPACE}" "Workspace provided is not a dir: ${WORKSPACE}" 2
@@ -24,8 +31,8 @@ is_defined "${JOB_NAME}" "No JOB_NAME provided." 3
 is_defined "${BUILD_ID}" "No BUILD_ID provided." 4
 
 readonly CONTAINER_COMMAND=${CONTAINER_COMMAND:-"${WORKSPACE}/hera/wait.sh"}
-readonly VOLUME_PATH="${VOLUME_HOME}/${JOB_NAME}"
 
+# shellcheck disable=SC2016
 run_ssh "podman run \
             --name $(container_name '${JOB_NAME}' '${BUILD_ID}') \
             --rm $(add_parent_volume_if_provided) \
