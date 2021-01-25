8731521082039c40e4119662b233ad2181a40e98
Romain Pelisse <belaran@gmail.com>
2021-01-25 16:49:35 +0100

    Fixes all shellcheck warnings on wait.sh

--- wait.sh
+++ wait.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 readonly BUILD_JOB_TIMEOUT=${BUILD_JOB_TIMEOUT:-'18000'}
 readonly PAUSE_LENGTH=${PAUSE_LENGTH:-'1'}
 
@@ -6,7 +8,7 @@ echo "Wait for ${BUILD_JOB_TIMEOUT}."
 echo -n ''
 count=${BUILD_JOB_TIMEOUT}
 while [ "${count}" -gt 0 ];
-  count=$(expr "${count}" '-' "${PAUSE_LENGTH}")
+   count=$(("${count}" - "${PAUSE_LENGTH}"))
 do
   echo -n '.'
   sleep "${PAUSE_LENGTH}"
