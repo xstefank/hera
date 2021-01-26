00f095db8ef8530bb798beeab267bcac1e981209
Romain Pelisse <belaran@gmail.com>
2021-01-26 14:09:48 +0100

    Refactor 'stop' to use container_name
    This removes the needs to keep track of the CID

--- stop.sh
+++ stop.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-readonly CID=${CID}
 readonly HERA_HOME=${HERA_HOME}
 set -euo pipefail
 
 # shellcheck source=library.sh
 source "${HERA_HOME}"/library.sh
 
-if [ -z "${CID}" ]; then
-  echo "INFO: No CID provided, skipping."
-else
-  run_ssh "podman stop -i ${CID}"
-fi
+readonly CONTAINER_TO_DELETE=$(container_name "${JOB_NAME}" "${BUILD_ID}")
+is_defined "${CONTAINER_TO_DELETE}" "Could not generate container name with provided JOB_NAME(${JOB_NAME}) and BUILD_ID(${BUILD_ID}."
+run_ssh "podman stop -i ${CONTAINER_TO_DELETE}"
