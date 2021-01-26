71db2aeb69b4db931f5a359939b9e659a7dab093
Romain Pelisse <belaran@gmail.com>
2021-01-26 14:23:56 +0100

    Rework job to use container_name (not cid)

--- job.sh
+++ job.sh
@@ -20,10 +20,9 @@ dumpBuildEnv() {
   fi
 }
 
-is_defined "${CID}" 'No contained id provided' 1
 is_defined "${BUILD_SCRIPT}" 'No build script provided' 2
 
-readonly SHORT_CID=$(echo "${CID}" | sed -e 's/\(^......\).*$/\1/')
+readonly CONTAINER_NAME=$(container_name "${JOB_NAME}" "${BUILD_ID}")
 
 dumpBuildEnv "${HERA_HOME}/build-env.sh"
 
@@ -35,4 +34,4 @@ run_ssh "podman exec \
 		-e MAVEN_OPTS='"${MAVEN_OPTS}"' \
 		-e BUILD_ID="${BUILD_ID}" \
 		-e MAVEN_SETTINGS_XML="${MAVEN_SETTINGS_XML}" \
-		-ti ${SHORT_CID} '${BUILD_SCRIPT}' ${@}"
+		-ti ${CONTAINER_NAME} '${BUILD_SCRIPT}' ${@}"
