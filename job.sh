62a8e2a39f6b761515f110818bb35d99d5104296
Romain Pelisse <belaran@gmail.com>
2021-03-11 21:18:12 +0100

    Cleanup SSH passed over vars

--- job.sh
+++ job.sh
@@ -33,10 +33,7 @@ run_ssh "podman exec \
         -e JAVA_HOME="${JAVA_HOME}" \
         -e HARMONIA_SCRIPT="${HARMONIA_SCRIPT}" \
         -e TO_ADDRESS="${TO_ADDRESS}" \
-        -e RULE_NAME="${RULE_NAME}" \
-        -e TO_ADDRESS="${TO_ADDRESS}" \
-        -e REPORT_TITLE="\'${REPORT_TITLE}\'" \
-        -e LOGGER_PROJECT_CODE="${LOGGER_PROJECT_CODE}" \
+        -e DEBUG="${DEBUG}" \
         -e MAVEN_HOME="${MAVEN_HOME}" \
         -e MAVEN_OPTS='"${MAVEN_OPTS}"' \
         -e MAVEN_GOALS='"${MAVEN_GOALS}"' \
