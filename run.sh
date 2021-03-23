b2e5e687149ba09bc6d4be4ac4ed7dc2ec8c620d
Romain Pelisse <belaran@gmail.com>
2021-03-23 21:05:29 +0100

    Switch to automation podman image

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +u
 readonly PARENT_JOB_VOLUME=${PARENT_JOB_VOLUME}
-readonly BUILD_PODMAN_IMAGE=${BUILD_PODMAN_IMAGE:-'automaton'}
+readonly BUILD_PODMAN_IMAGE=${BUILD_PODMAN_IMAGE:-'localhost/automatons'}
 readonly CONTAINER_USER=${CONTAINER_USER:-'jenkins'}
 readonly JENKINS_HOME_DIR=${JENKINS_HOME_DIR:-'/home/jenkins/'}
 readonly JOB_NAME=${JOB_NAME}
